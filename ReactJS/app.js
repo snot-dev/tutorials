9f790f76314ce2ff840b9d0421029be57e1911b9
goncalo.assuncao <goncalo.assuncao@enear.co>
2016-06-15 19:15:53 +0100

    React - github form cards

--- ReactJS/app.js
+++ ReactJS/app.js
@@ -3,11 +3,12 @@ var app = express();
 var pahty = require('path');
 var index = require('./route/index');
 
-
-app.set('views', pahty.join(__dirname, 'public'));
-app.set('view engine', 'hbs')
 app.use(express.static(pahty.join(__dirname, 'public')));
+app.set('views', pahty.join(__dirname, 'public/views'));
+app.set('view engine', 'hbs')
 
 app.use('/', index);
 
-app.listen('3000');
+app.listen('3000', function() {
+    console.log("Listening ...");
+});
--- ReactJS/public/main.js
+++ ReactJS/public/main.js
@@ -1,11 +1,59 @@
-var Button = React.createClass({
+var Card = React.createClass({
+    getInitialState: function(){
+        return {};
+    },
+    componentDidMount: function( ){
+        var component = this;
+        $.get("https://api.github.com/users/" + this.props.login, function(data){
+            component.setState(data);
+        });
+    },
+    render: function(){
+        return (
+            <div>
+            <img src={this.state.avatar_url} width="80"/>
+            <h3>{this.state.login}</h3>
+            <hr />
+            </div>
+        );
+    }
+})
+
+var Form = React.createClass({
+    handleSubmit: function(e) {
+        e.preventDefault();
+        var loginInput = this.refs.login;
+        this.props.addCard(loginInput.value);
+        loginInput.value = '';
+    },
     render: function() {
         return (
-            <button> GO </button>
+            <form onSubmit={this.handleSubmit}>
+                <input placeholder="github login" ref="login"/>
+                <button>Add</button>
+            </form>
+        )
+    }
+});
+
+var Main = React.createClass({
+    getInitialState: function() {
+        //return{logins: ['zpao', 'fisherwebdev']};
+        return{logins: []};
+    },
+    addCard: function(loginToAdd) {
+        this.setState({logins: this.state.logins.concat(loginToAdd)});
+    },
+    render: function(){
+        var cards = this.state.logins.map(function(login) {
+            return (<Card login={login} />)
+        });
+        return (
+            <div>
+                <Form addCard={this.addCard}/>
+            </div>
         );
     }
 });
 
-
-alert("Cona");
-ReactDOM.render(<Button />, document.getElementById('container'));
+ReactDOM.render(<Main />, document.getElementById('container'));
